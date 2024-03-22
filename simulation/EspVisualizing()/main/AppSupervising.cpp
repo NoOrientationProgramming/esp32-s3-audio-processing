cf9ba736256850442a0511085fcf5887221f6c7c
Johannes Natter <office@dsp-crowd.com>
2024-03-22 23:40:22 +0100

    AppSupervising(): Enable core dump

--- simulation/EspVisualizing()/main/AppSupervising.cpp
+++ simulation/EspVisualizing()/main/AppSupervising.cpp
@@ -23,9 +23,13 @@
   along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <unistd.h>
+#include <signal.h>
+
 #include "AppSupervising.h"
 #include "SystemDebugging.h"
 #include "EspSimulating.h"
+#include "LibFilesys.h"
 
 #define dForEach_ProcState(gen) \
 		gen(StStart) \
@@ -44,12 +48,93 @@ using namespace std;
 
 #define LOG_LVL	0
 
+static Processing *pTreeRoot = NULL;
+static char nameApp[16];
+static char nameFileProc[64];
+static char buffProcTree[2*1024*1024];
+static bool procTreeSaveInProgress = false;
+
 AppSupervising::AppSupervising()
 	: Processing("AppSupervising")
 	, mStartMs(0)
 	, mpSimu(NULL)
 {
 	mState = StStart;
+}
+
+static void procTreeSave()
+{
+	time_t now;
+	FILE *pFile;
+	int res;
+	size_t lenReq, lenDone;
+
+	if (procTreeSaveInProgress)
+		return;
+	procTreeSaveInProgress = true;
+
+	now = time(NULL);
+
+	if (!pTreeRoot)
+	{
+		wrnLog("process tree root not defined");
+		return;
+	}
+
+	*buffProcTree = 0;
+
+	pTreeRoot->processTreeStr(
+		buffProcTree,
+		buffProcTree + sizeof(buffProcTree),
+		true,
+		true);
+
+	res = snprintf(nameFileProc, sizeof(nameFileProc),
+			"%ld_%s_tree-proc.txt",
+			now,
+			nameApp);
+	if (res < 0)
+	{
+		wrnLog("could not create process tree file name");
+		return;
+	}
+
+	pFile = fopen(nameFileProc, "w");
+	if (!pFile)
+	{
+		wrnLog("could not open process tree file");
+		return;
+	}
+
+	lenReq = strlen(buffProcTree);
+	lenDone = fwrite(buffProcTree, 1, lenReq, pFile);
+
+	if (lenDone != lenReq)
+	{
+		wrnLog("error writing to process tree file");
+		return;
+	}
+
+	fclose(pFile);
+}
+
+// - https://man7.org/linux/man-pages/man5/core.5.html
+// - https://man7.org/linux/man-pages/man7/signal.7.html
+// - https://man7.org/linux/man-pages/man3/abort.3.html
+void coreDumpRequest(int signum)
+{
+	wrnLog("Got signal: %d", signum);
+
+	if (signum != SIGABRT)
+	{
+		wrnLog("Requesting core dump");
+		abort();
+
+		return;
+	}
+
+	wrnLog("Creating process tree file");
+	procTreeSave();
 }
 
 /* member functions */
@@ -59,12 +144,20 @@ Success AppSupervising::process()
 	//uint32_t curTimeMs = millis();
 	//uint32_t diffMs = curTimeMs - mStartMs;
 	Success success;
+	bool ok;
 #if 0
 	dStateTrace;
 #endif
 	switch (mState)
 	{
 	case StStart:
+
+		pTreeRoot = this;
+		signal(SIGABRT, coreDumpRequest);
+
+		ok = coreDumpsEnable(coreDumpRequest);
+		if (!ok)
+			procWrnLog("could not enable core dumps");
 
 		dbgStart();
 
