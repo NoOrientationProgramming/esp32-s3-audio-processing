14b3ee7799559bfe989aca5f1fb5f8af107955a2
Johannes Natter <office@dsp-crowd.com>
2024-03-26 23:45:11 +0100

    SignalGenerating(): Add

---
+++ simulation/EspVisualizing()/main/SignalGenerating.h
@@ -0,0 +1,72 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 26.03.2024
+
+  Copyright (C) 2024-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SIGNAL_GENERATING_H
+#define SIGNAL_GENERATING_H
+
+#include "Processing.h"
+
+class SignalGenerating : public Processing
+{
+
+public:
+
+	static SignalGenerating *create()
+	{
+		return new (std::nothrow) SignalGenerating;
+	}
+
+protected:
+
+	SignalGenerating();
+	virtual ~SignalGenerating() {}
+
+private:
+
+	SignalGenerating(const SignalGenerating &) : Processing("") {}
+	SignalGenerating &operator=(const SignalGenerating &) { return *this; }
+
+	/*
+	 * Naming of functions:  objectVerb()
+	 * Example:              peerAdd()
+	 */
+
+	/* member functions */
+	Success process();
+	void processInfo(char *pBuf, char *pBufEnd);
+
+	/* member variables */
+	uint32_t mStartMs;
+
+	/* static functions */
+
+	/* static variables */
+
+	/* constants */
+
+};
+
+#endif
+
